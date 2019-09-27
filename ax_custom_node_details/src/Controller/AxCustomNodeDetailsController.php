a27728a9912b4032b36709d7129dd99065c6c065
amolpalhade <amol.palhade17@gmail.com>
2019-09-27 12:00:28 +0530

    Competed Requirement No 6
    Completed Requirement No 6 ie 
    "This module also provides a URL that responds with a JSON representation of a given node with the content type "page" only if the previously submitted API Key and a node id (nid) of an appropriate node are present, otherwise it will respond with "access denied"

--- ax_custom_node_details/src/Controller/AxCustomNodeDetailsController.php
+++ ax_custom_node_details/src/Controller/AxCustomNodeDetailsController.php
@@ -3,6 +3,7 @@
 namespace Drupal\ax_custom_node_details\Controller;
 // Importing the Namespaces for Controller
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
 
 
 /**
@@ -14,10 +15,39 @@ class AxCustomNodeDetailsController extends ControllerBase {
    * @param - Site API Key
    * @param - node Id
    */
-   public function nodeDetailAPI($site_api,$nid){
-     $response = ['message' => 'Hello, this is a rest service from Controller','site_api' => $site_api, 'nid' => $nid];
-     echo "hasd";
-     print_r($response);die();
+   public function nodeDetailAPI($requested_site_api,$requested_nid){
+     $data = array();
+     $response = array();
+     //$requested_site_api = "";$requested_nid = "";
+     if($requested_site_api!= "" && $requested_nid!= ""){
+       $site_config_site_api = \Drupal::config('system.site')->get('siteapikey');
+       //echo "THe valus is :-".$site_config_site_api;
+       if($requested_site_api != $site_config_site_api){
+         $data = "Invalid Site API Key";
+       }else{
+         $node = Node::load($requested_nid);
+         //print_r($node);
+         if(!empty($node)){
+           $node_type = $node->bundle();
+           //$node_type = $node->type->entity->label();
+           //echo "The node type is :- ".$node_type;
+           if($node_type == "page"){
+             $data = $node;
+           }else{
+             $data = "Access Denied";
+           }
+         }else{
+           //echo "Node Not Found";
+           $data  = "Node Not Found";
+         }
+       }
+     }else{
+       $data = "No Site API Key or Nid Found in URL";
+     }
+     $response['data'] = $data;
+     //echo "=====";
+     //print_r($response);
+     //die();
      return $response;
   }
 }
